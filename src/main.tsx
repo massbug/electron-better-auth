0e747527efa98c562c181c1b870f2523898ff337
cfngc4594 <cfngc4594@gmail.com>
2025-12-31 22:21:43 +0800

    feat(ui): add theme provider with dark mode support

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from './components/theme-provider.tsx'
 import { queryClient } from './lib/trpc.ts'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
